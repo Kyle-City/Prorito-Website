07a951574e0b51d948d4c73469065ddf446663f7
Kyle-City <1424940265@qq.com>
2026-03-11 16:48:01 +0800

    更新图片和产品信息

---
+++ Manual.js
@@ -0,0 +1,117 @@
+const manualData = [
+  {
+    id: 1,
+    name: "四头灯太空灯",
+    type: "产品说明书",
+    version: "1.0.0",
+    updateTime: "2025-02-09",
+    files: [
+      "doc/产品说明书/（保锐图）SKYFLOWER Series Prt-DT4-720W Space Light Operating Instructions.pdf",
+      "doc/产品说明书/产品说明书（保锐图）SKYFLOWER系列太空灯操作说明书综合版20210617.pdf"
+    ]
+  },
+  {
+    id: 23,
+    name: "FDQ-H08信号放大器",
+    type: "产品说明书",
+    version: "1.0.0",
+    updateTime: "2025-02-09",
+    files: [
+      "doc/产品说明书/保锐图信号放大器FDQ-H08-中文说明书20251208.pdf"
+    ]
+  },
+  {
+    id: 25,
+    name: "KZT-2600数字调光台",
+    type: "产品说明书",
+    version: "1.0.0",
+    updateTime: "2025-02-09",
+    files: [
+      "doc/产品说明书/KZT-2600调光台说明书20250412.pdf"
+    ]
+  },
+  {
+    id: 26,
+    name: "KT-2601数字调光台",
+    type: "产品说明书",
+    version: "1.0.0",
+    updateTime: "2025-02-09",
+    files: [
+      "doc/产品说明书/KT-2601调光台-中文说明书251223.pdf"
+    ]
+  },
+  {
+    id: 28,
+    name: "48路 智能调光台",
+    type: "产品说明书",
+    version: "1.0.0",
+    updateTime: "2025-02-09",
+    files: [
+      "doc/产品说明书/SC-K480智能调光台-中文说明书210409.pdf"
+    ]
+  },
+  {
+    id: 30,
+    name: "会议室墙面嵌入式触摸调光台",
+    type: "产品说明书",
+    version: "1.0.0",
+    updateTime: "2025-02-09",
+    files: [
+      "doc/产品说明书/ZK-606全屏会议灯光总控面板使用说明.pdf"
+    ]
+  },
+  {
+    id: 35,
+    name: "SF系列 演播室平板柔光灯 SF150DPro/SF150Bi Pro",
+    type: "产品说明书",
+    version: "1.0.0",
+    updateTime: "2025-02-09",
+    files: [
+      "doc/产品说明书/LP-SF150DPro-中文说明书20251208.pdf",
+      "doc/产品说明书/LP-SF150Bi Pro-中文说明书20251208.pdf"
+    ]
+  },
+  {
+    id: 37,
+    name: "G系列 SP-G100Pro 演播室聚光灯",
+    type: "产品说明书",
+    version: "1.0.0",
+    updateTime: "2025-02-09",
+    files: [
+      "doc/产品说明书/SP-G100Pro-中文说明书20251208.pdf"
+    ]
+  },
+  {
+    id: 39,
+    name: "S系列 双色温影视柔光灯S120 Bi",
+    type: "产品说明书",
+    version: "1.0.0",
+    updateTime: "2025-02-09",
+    files: [
+      "doc/产品说明书/Light Panel S120Bi中文使用说明书.pdf"
+    ]
+  },
+  {
+    id: 100,
+    name: "SP系列 双色温影视聚光灯",
+    type: "产品说明书",
+    version: "1.0.0",
+    updateTime: "2025-02-09",
+    files: [
+      "doc/产品说明书/SP-J100Pro-中文说明书20251208.pdf"
+    ]
+  },
+  {
+    id: 101,
+    name: "RAYTU-512数字调光台",
+    type: "产品说明书",
+    version: "1.0.0",
+    updateTime: "2025-02-09",
+    files: [
+      "doc/产品说明书/RAYTU-512调光台中文说明书.pdf"
+    ]
+  }
+];
+
+// 如果后续需要动态排序，可以使用这个方法
+// const sortedManualData = manualData.sort((a, b) => a.id - b.id);
